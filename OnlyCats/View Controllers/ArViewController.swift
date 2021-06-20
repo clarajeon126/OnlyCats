ab6ffdc3ed148a7eb21cd63722ea08d239692647
clarajeon <clarajeon126@gmail.com>
2021-06-20 04:43:30 -0400

    ar view added

---
+++ OnlyCats/View Controllers/ArViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ArViewController.swift
+//  OnlyCats
+//
+//  Created by Clara Jeon on 6/20/21.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ArViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
