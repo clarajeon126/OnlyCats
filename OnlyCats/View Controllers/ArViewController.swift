abd083bd9c773b7eb3763c8a4fa3a621cb00b77f
clarajeon <clarajeon126@gmail.com>
2021-06-20 05:40:45 -0400

    3d works

--- OnlyCats/View Controllers/ArViewController.swift
+++ OnlyCats/View Controllers/ArViewController.swift
@@ -9,16 +9,67 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ArViewController: UIViewController {
+class ArViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.showsStatistics = false
+        
+        
+        
+        let scene = SCNScene()
+        sceneView.scene = scene
 
-        // Do any additional setup after loading the view.
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        
+        sceneView.session.run(configuration)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+        guard let hitResult = result.last else {
+            return
+        }
+        let hitTransform = SCNMatrix4.init(hitResult.worldTransform)
+        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        print(hitVector)
+        createCat(position: hitVector)
+        
+    }
+    
+    func createCat(position: SCNVector3){
+        var catNode = collada2SCNNode(filepath: "art.scnassets/leximodel/lexi.dae")
+
+        catNode.position = position
+        
+        print("here")
+        sceneView.scene.rootNode.addChildNode(catNode)
+    }
+    
+    func collada2SCNNode(filepath:String) -> SCNNode {
+        var node = SCNNode()
+        let scene = SCNScene(named: filepath)
+        var nodeArray = scene!.rootNode.childNodes
+
+        for childNode in nodeArray {
+            node.addChildNode(childNode as SCNNode)
+        }
+        return node
+    }
 
     /*
     // MARK: - Navigation
